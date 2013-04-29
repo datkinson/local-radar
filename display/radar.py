855606f84d05f09527c464e62acc7d1de4cef063
Daniel Atkinson <daa9@aber.ac.uk>
2013-04-29 20:21:05 +0100

    Working database reading and page rendering

--- display/radar.py
+++ display/radar.py
@@ -1,9 +1,11 @@
-from flask import Flask, g
+from flask import Flask, g, jsonify, render_template
 import sqlite3
 
 app = Flask(__name__)
 
 DATABASE = 'db/sensors.db'
+
+output = ""
 
 def connect_db():
     return sqlite3.connect(DATABASE)
@@ -16,13 +18,18 @@ def before_request():
 def teardown_request(exception):
     if hasattr(g, 'db'):
         g.db.close()
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
 
 @app.route('/')
 def showall():
-    cur = g.db.execute('select * from sonar')
-#    for row in cur.fetchall():
-#	    print row['distance']
-    print cur.fetchall()
+    g.db.row_factory = dict_factory
+    cur = g.db.cursor()
+    cur.execute("select * from sonar")
+    return render_template("sonar.html", readings = cur.fetchall())
 
 if __name__ == '__main__':
     app.debug = True
