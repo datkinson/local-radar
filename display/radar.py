fb7d0588c15205b4f7ee8a6826e3f55d9e8a0768
Daniel Atkinson <dan@hourd.net>
2013-04-29 18:31:00 +0100

    attempt to fix database printing

--- display/radar.py
+++ display/radar.py
@@ -17,19 +17,11 @@ def teardown_request(exception):
     if hasattr(g, 'db'):
         g.db.close()
 
-def query_db(query, args=(), one=False):
-    cur = g.db.execute(query, args)
-    rv = [dict((cur.description[idx][0], value)
-        for idx, value in enumerate(row)) for row in cur.fetchall()]
-    return (rv[0] if rv else None) if one else rv
-
 @app.route('/')
 def showall():
-    readings = query_db('select * from sonar where Id = 1')
-    if readings is None:
-        print 'no readings'
-    else:
-        print readings['distance']
+    cur = g.db.execute('select * from sonar')
+    for row in cur.fetchall():
+	    print row['distance']
 
 if __name__ == '__main__':
     app.debug = True
